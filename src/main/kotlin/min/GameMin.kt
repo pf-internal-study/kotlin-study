198942f1b3bfba7fbc747eeb3bf194678dc37dba
minyeol2 <my.lee@pet-frineds.co.kr>
2024-08-28 23:28:03 +0900

    refactor : 게터 재정의 (불변리스트 & 백킹필드 사용)

--- src/main/kotlin/min/GameMin.kt
+++ src/main/kotlin/min/GameMin.kt
@@ -1,25 +1,28 @@
 package internal.study.kotlin.min
 
 class GameMin {
-    private val rounds: MutableList<RoundMin> = MutableList(10) { RoundMin() }
-    private val players: MutableList<PlayerMin> = mutableListOf()
+    private val rounds: List<RoundMin> = List(10) { RoundMin() }
+    private val _players: MutableList<PlayerMin> = mutableListOf()
     private var winner: PlayerMin? = null
+
+    val players: List<PlayerMin>
+        get() = _players.toList()
 
     /* 게임 참가 */
     fun join(player: PlayerMin) {
-        if (players.contains(player)) {
+        if (_players.contains(player)) {
             throw IllegalArgumentException("${player.name}는 이미 참여하였습니다.")
         }
-        players.add(player)
+        _players.add(player)
     }
 
     /* 게임 시작 */
     fun start() {
-        if (players.isEmpty()) {
+        if (_players.isEmpty()) {
             throw IllegalStateException("참가자가 없습니다.")
         }
 
-        rounds.forEach { round -> round.playRound(players) }
+        rounds.forEach { round -> round.playRound(_players) }
         determineFinalWinner()
     }
 
@@ -30,6 +33,6 @@ class GameMin {
 
     /* 최종 우승자 반환 */
     fun getFinalWinner(): PlayerMin {
-        TODO("라운드 승자 리턴")
+        return winner ?: throw IllegalStateException("게임이 아직 끝나지 않았습니다.")
     }
 }
