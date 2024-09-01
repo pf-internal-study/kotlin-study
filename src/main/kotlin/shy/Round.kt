67033129ad387a9070f2c59d25ea52d44d16b0c1
leejg3144 <jaosn0620@gmail.com>
2024-09-01 21:36:10 +0900

    fix : 코드 수정 - 동점자 대상 처리 수정

--- src/main/kotlin/shy/Round.kt
+++ src/main/kotlin/shy/Round.kt
@@ -7,7 +7,7 @@ class Round {
     val records: Map<Player, List<Int>>
         get() = _records.toMap()
 
-    fun run(players: List<Player>): Player{
+    fun run(players: List<Player>): Player {
         players.forEach { player ->
             player.rollDice()
             _records[player] = player.getEyes()
@@ -16,7 +16,7 @@ class Round {
         printRound()
 
         // 재수행 검증 처리
-        val sameScorePlayers = checkSameScore()
+        val sameScorePlayers = checkSameScore(players)
         if (sameScorePlayers.size > 1) {
             run(sameScorePlayers)
         }
@@ -28,6 +28,12 @@ class Round {
         return winner!!;
     }
 
+    /**
+     * 승자를 구한다.
+     * 1. 요청받은 플레이어를 필터
+     * 2. 플레이어의 주사위 최대 값 확인
+     * 3. 승자Player 반환
+     */
     private fun win(players: List<Player>) {
         winner = _records
             .filterKeys { it in players }
@@ -41,10 +47,19 @@ class Round {
         }
     }
 
-    private fun checkSameScore(): List<Player> {
-        val sumMap = _records.mapValues { (_, record) -> record.sum() }
-        val maxSum = sumMap.values.maxOrNull() ?: 0
+    /***
+     * 1. 요청된 플레이어 기준으로 필터처리
+     * 2. 최대값을 구한다.
+     * 2. 동점자를 리턴한다.
+     */
+    private fun checkSameScore(players: List<Player>): List<Player> {
+        val target = _records.filterKeys { it in players }
 
-        return sumMap.filter { it.value == maxSum }.keys.toList()
+        val maxSum = target.map{ it.value.sum() }
+            .maxOrNull() ?: return emptyList()
+
+        return target.filterValues { it.sum() == maxSum }
+            .keys
+            .toList()
     }
 }
