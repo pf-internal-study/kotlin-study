239582075f36d4e4cf1b8c970793987a9794bbcc
leejg3144 <jaosn0620@gmail.com>
2024-09-01 20:51:00 +0900

    feat: day4 mission Add

--- src/main/kotlin/shy/Game.kt
+++ src/main/kotlin/shy/Game.kt
@@ -1,15 +1,21 @@
 package internal.study.kotlin.shy
 
 
-class Game{
-    private val rounds: List<Round> = List(ROUNDS_COUNT) { Round() }
+class Game {
+    private val _rounds: List<Round> = List(ROUNDS_COUNT) { Round() }
     private val _players: MutableList<Player> = mutableListOf()
     var winner: Player? = null
 
     val players: List<Player>
         get() = _players.toList()
 
+    val rounds: List<Round>
+        get() = _rounds.toList()
 
+
+    fun join(players: List<Player>) {
+        players.forEach {player -> join(player)}
+    }
     /**
      * 게임에 참가
      */
@@ -20,6 +26,13 @@ class Game{
         _players.add(player)
     }
 
+    fun start() {
+        rounds.forEach { round ->
+            println("------------------ start -------------------")
+            round.run(_players)
+            println("------------------ end -------------------")
+        }
+    }
     /***
      * 승리자를 확인
      */
--- src/main/kotlin/shy/Round.kt
+++ src/main/kotlin/shy/Round.kt
@@ -7,11 +7,44 @@ class Round {
     val records: Map<Player, List<Int>>
         get() = _records.toMap()
 
-    fun join(player: Player) {
-        TODO("다음 기회에")
+    fun run(players: List<Player>): Player{
+        players.forEach { player ->
+            player.rollDice()
+            _records[player] = player.getEyes()
+        }
+
+        printRound()
+
+        // 재수행 검증 처리
+        val sameScorePlayers = checkSameScore()
+        if (sameScorePlayers.size > 1) {
+            run(sameScorePlayers)
+        }
+        else {
+            win(players)
+        }
+
+        println("라운드 승자는 ${winner!!.name} 입니다.")
+        return winner!!;
     }
 
-    fun win() {
-        TODO("다음 기회에")
+    private fun win(players: List<Player>) {
+        winner = _records
+            .filterKeys { it in players }
+            .maxByOrNull { (_, diceResults) -> diceResults.sum() }
+            ?.key
+    }
+
+    private fun printRound() {
+        _records.forEach { (player, diceResults) ->
+            println("플레이어[${player.name}]: ${diceResults.sum()}")
+        }
+    }
+
+    private fun checkSameScore(): List<Player> {
+        val sumMap = _records.mapValues { (_, record) -> record.sum() }
+        val maxSum = sumMap.values.maxOrNull() ?: 0
+
+        return sumMap.filter { it.value == maxSum }.keys.toList()
     }
 }
