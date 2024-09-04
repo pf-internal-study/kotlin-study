6360bdf4c024fab86f1835d13160b2b4f7301bcb
minyeol2 <my.lee@pet-frineds.co.kr>
2024-09-04 19:39:46 +0900

    feat : 라운드 결과 로그 출력

--- src/main/kotlin/min/RoundMin.kt
+++ src/main/kotlin/min/RoundMin.kt
@@ -13,6 +13,7 @@ class RoundMin {
             val diceResults = player.rollDice()
             _records[player] = diceResults
         }
+        print(_records)
         val currentRoundRecords = _records.filterKeys { it in players }
         val maxScore = currentRoundRecords.maxOfOrNull { (_, diceResults) -> diceResults.sum() }
             ?: throw IllegalStateException("점수를 계산할 수 없습니다.")
@@ -20,6 +21,7 @@ class RoundMin {
 
         if (candidates.size == 1) {
             winner = candidates.first()
+            println("우승자!: ${winner?.name} 점수: ${currentRoundRecords[winner]?.sum()}")
             return
         }
         run(candidates.toList())
@@ -28,5 +30,12 @@ class RoundMin {
     /* 라운드 승자 리턴 */
     fun getRoundWinner(): PlayerMin {
         return winner ?: throw IllegalStateException("승자가 정해지지 않았습니다.")
+    }
+
+    fun print(records : MutableMap<PlayerMin, List<Int>>) {
+        println("라운드 결과:")
+        records.forEach { (player, results) ->
+            println("${player.name}: ${results.sum()} (${results.joinToString(", ")})")
+        }
     }
 }
